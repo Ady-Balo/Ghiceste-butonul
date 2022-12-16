37cbb396e3648396c1c0e6e27c98bf1e6065b7d3
Adorjan Balo <119331480+Ady-Balo@users.noreply.github.com>
2022-12-16 18:06:22 +0200

    Add files via upload

---
+++ Guess the button simple/script.js
@@ -0,0 +1,19 @@
+    
+        var correctButton = 0
+
+function selectCorrectButton() {
+    correctButton = Math.floor((Math.random() * 3) + 1)
+}
+
+function showMessage(clicked_id) {
+    if (clicked_id == correctButton) {
+        document.getElementById("message").innerHTML = "👍You win!"
+        document.getElementById("message").style.color = "green"
+        document.getElementById("1").disabled = "true"
+        document.getElementById("2").disabled = "true"
+        document.getElementById("3").disabled = "true"
+    } else {
+        document.getElementById("message").innerHTML = "😞 Try again!"
+        document.getElementById("message").style.color = "red"
+    }
+}
